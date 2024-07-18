c0804b9791862ca42bb012a2a6ce36afe999d1c5
Derevjannykh Ivan <ivandubochit@gmail.com>
2024-07-18 14:21:10 +0300

    feat: closing the burger when clicking outside the menu + overlay

--- shelter/js/burger.js
+++ shelter/js/burger.js
@@ -1,13 +1,13 @@
+const body = document.body;
 const menuBtn = document.querySelector('.burger__btn');
 const menuMobile = document.querySelector('.header__list');
-const body = document.body;
 const navItem = menuMobile.querySelectorAll('.header__item');
-
 
 menuBtn.addEventListener('click',()=> {
 	body.classList.toggle('scroll--stop')
 	menuBtn.classList.toggle('burger--transform');
 	menuMobile.classList.toggle('burger--open');
+	body.classList.toggle('body--overlay');
 });
 
 navItem.forEach(element => {
@@ -15,5 +15,16 @@ navItem.forEach(element => {
 		body.classList.remove('scroll--stop')
 		menuBtn.classList.remove('burger--transform');
 		menuMobile.classList.remove('burger--open');
+		body.classList.remove('body--overlay');
 	});
+});
+
+document.addEventListener('click', (event) => {
+	const target = event.target;
+	if (!menuMobile.contains(target) && !menuBtn.contains(target)) {
+			body.classList.remove('scroll--stop');
+			menuBtn.classList.remove('burger--transform');
+			menuMobile.classList.remove('burger--open');
+			body.classList.remove('body--overlay');
+	}
 });
