35e2715ac0f70c87788416c664411cc91a29d7cc
Derevjannykh Ivan <ivandubochit@gmail.com>
2024-08-01 10:15:14 +0400

    feat: add generating cards using js

---
+++ shelter/js/card.js
@@ -0,0 +1,46 @@
+import { petsData } from './arr-pets.js';
+
+export function initCard() {
+	document.addEventListener("DOMContentLoaded", () => {
+    const petsCardInner = document.querySelector(".pets__card-inner");
+
+    petsData.forEach(pet => {
+        const petCard = document.createElement("article");
+        petCard.classList.add("pets__card");
+
+        const petImageContainer = document.createElement("div");
+        petImageContainer.classList.add("pets__card-image");
+
+        const petImage = document.createElement("img");
+        petImage.classList.add("image");
+        petImage.src = pet.img;
+        petImage.alt = pet.name;
+
+        petImageContainer.appendChild(petImage);
+
+        const petContent = document.createElement("div");
+        petContent.classList.add("pets__content");
+
+        const petName = document.createElement("h3");
+        petName.classList.add("pets__content-title");
+        petName.textContent = pet.name;
+
+        const petButton = document.createElement("button");
+        petButton.classList.add("pets__btn-more");
+        petButton.type = "button";
+        petButton.textContent = "Learn more";
+
+        petButton.addEventListener("click", () => {
+            showPetDetails(pet);
+        });
+
+        petContent.appendChild(petName);
+        petContent.appendChild(petButton);
+
+        petCard.appendChild(petImageContainer);
+        petCard.appendChild(petContent);
+
+        petsCardInner.appendChild(petCard);
+    });
+	});
+}
--- shelter/js/index.js
+++ shelter/js/index.js
@@ -1,5 +1,8 @@
 import { initBurger } from './burger.js';
-import { initPopUp } from './pop-up.js'
+import { initPopUp } from './pop-up.js';
+import { initCard } from './card.js';
+
 
 initBurger();
 initPopUp();
+initCard();
