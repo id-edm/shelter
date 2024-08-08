4322a22b1a1c75c33501457c85810cfabf642bc7
id-edm <ivandubochit@gmail.com>
2024-08-08 21:25:56 +0400

    fix: content jitter when opening modal window

--- shelter/js/pop-up.js
+++ shelter/js/pop-up.js
@@ -1,30 +1,42 @@
 
 function initPopUp() {
-	document.addEventListener("DOMContentLoaded", () => {
-		const body = document.body
-		const popUp = document.querySelector(".popup")
-		const cardPets = document.querySelectorAll(".pets__btn-more")
+  document.addEventListener("DOMContentLoaded", () => {
+    const body = document.body;
+    const popUp = document.querySelector(".popup");
+    const cardPets = document.querySelectorAll(".pets__btn-more");
 
-		function toggleClassesPopUp() {
-			body.classList.toggle("scroll--stop")
-			popUp.classList.toggle("popup--hidden")
-		}
+    function toggleClassesPopUp() {
+      const scrollbarWidth = getScrollbarWidth();
+        if (popUp.classList.contains("popup--hidden")) {
+          body.style.paddingRight = `${scrollbarWidth}px`;
+          body.classList.add("scroll--stop");
+        } else {
+          body.style.paddingRight = "0px";
+          body.classList.remove("scroll--stop");
+        };
 
-		cardPets.forEach(card => {
-			card.addEventListener("click", () => {
-				toggleClassesPopUp()
-			})
-		})
+      popUp.classList.toggle("popup--hidden");
+    };
 
-		popUp.addEventListener("click", event => {
-			if (
-				event.target.classList.contains("popup") ||
-				event.target.classList.contains("popup__close-btn")
-			) {
-				toggleClassesPopUp()
-			}
-		});
-	});
+    function getScrollbarWidth() {
+      return window.innerWidth - document.documentElement.clientWidth;
+    };
+
+    cardPets.forEach(card => {
+      card.addEventListener("click", () => {
+        toggleClassesPopUp()
+      });
+    });
+
+    popUp.addEventListener("click", event => {
+      if (
+        event.target.classList.contains("popup") ||
+        event.target.classList.contains("popup__close-btn")
+      ) {
+        toggleClassesPopUp()
+      }
+    });
+  });
 };
 
 export { initPopUp };
