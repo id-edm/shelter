43d69da3653736e026360ba28bd9a3b5b0102ae7
Derevjannykh Ivan <ivandubochit@gmail.com>
2024-09-06 20:15:30 +0400

    refactor: removed code duplication

--- shelter/js/burger.js
+++ shelter/js/burger.js
@@ -1,39 +1,4 @@
 
-const body = document.body;
-const menuBtn = document.querySelector('.burger__btn');
-const menuMobile = document.querySelector('.header__list');
-const navItem = menuMobile.querySelectorAll('.header__item');
-
-	function toggleClassesBurger() {
-		body.classList.toggle("scroll--stop");
-		menuBtn.classList.toggle("burger--transform");
-		menuMobile.classList.toggle("burger--open");
-		body.classList.toggle("body--overlay");
-	}
-
-	menuBtn.addEventListener("click", () => {
-		toggleClassesBurger()
-	})
-
-navItem.forEach(element => {
-	element.addEventListener("click",()=> {
-		body.classList.remove('scroll--stop')
-		menuBtn.classList.remove('burger--transform');
-		menuMobile.classList.remove('burger--open');
-		body.classList.remove('body--overlay');
-	});
-});
-
-document.addEventListener('click', (event) => {
-	const target = event.target;
-	if (!menuMobile.contains(target) && !menuBtn.contains(target)) {
-			body.classList.remove('scroll--stop');
-			menuBtn.classList.remove('burger--transform');
-			menuMobile.classList.remove('burger--open');
-			body.classList.remove('body--overlay');
-	}
-});
-
 function initBurger() {
 	const body = document.body
 	const menuBtn = document.querySelector(".burger__btn");
