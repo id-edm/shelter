b0432a18290d380af6cc94f4a03254157dc32268
id-edm <ivandubochit@gmail.com>
2024-09-17 23:13:28 +0400

    refactor: add properties audioTracks

--- audio-player/js/audio.js
+++ audio-player/js/audio.js
@@ -3,18 +3,22 @@ const audioTracks = {
       {
         artist: 'Linkin Park',
         title: 'Numb',
+        track: '../assets/tracks/Linkin Park - Faint.mp3',
         background: '../assets/img/bg-player/linkin-park.jpg',
         cover: '../assets/img/cover/meteora.jpg',
       },
       {
         artist: 'Limp Bizkit',
-        title: 'Rollin',
+        title: 'Rollin\'',
+        track: '../assets/tracks/Limp Bizkit - Rolin\'.mp3',
         background: '../assets/img/bg-player/limp-bizkit.jpg',
         cover: '../assets/img/cover/rollin.jpg',
       },
       {
         artist: 'Rammstein',
         title: 'Deutschland',
+        track: '../assets/tracks/Rammstein - Deutschland.mp3',
+        track: '../assets/tracks/Linkin Park - Faint.mp3',
         background: '../assets/img/bg-player/rammstein.jpg',
         cover: '../assets/img/cover/deutschland.jpg',
       }
