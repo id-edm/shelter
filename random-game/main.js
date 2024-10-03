c24ca2478c6db12d6568cb449fd28232fcb58e93
Derevjannykh Ivan <ivandubochit@gmail.com>
2024-10-03 13:11:44 +0400

    feat: add food and mechanics of eating snake

--- random-game/main.js
+++ random-game/main.js
@@ -4,14 +4,16 @@ const ctx = canvas.getContext("2d")
 const cageSize = 20
 const canvasWidth = 320
 const canvasHeight = 400
-
-const position = {
-	x: Math.floor(Math.random() * (canvasWidth / cageSize)) * cageSize,
-	y: Math.floor(Math.random() * (canvasHeight / cageSize)) * cageSize,
-}
-
-let snake = [{ x: position.x, y: position.y }];
+let snake = [{ x: randomPosition().x, y: randomPosition().y }];
 let directionSnake = {x: 0, y: 0 };
+let food = randomPosition();
+
+function randomPosition() {
+	return {
+		x: Math.floor(Math.random() * (canvasWidth / cageSize)) * cageSize,
+		y: Math.floor(Math.random() * (canvasHeight / cageSize)) * cageSize,
+	}
+}
 
 function changeDirection(event) {
 	const key = event.key;
@@ -36,18 +38,22 @@ function changeDirection(event) {
 document.addEventListener('keydown', changeDirection);
 
 function moveSnake() {
-	// получаем новую позицию головы змеи
+	//получаем новую позицию головы змеи
 	const newHead = {
-			x: snake[0].x + directionSnake.x,
-			y: snake[0].y + directionSnake.y,
+		x: snake[0].x + directionSnake.x,
+		y: snake[0].y + directionSnake.y,
 	};
-
+	//проверка, съела ли змея еду
+	if (newHead.x === food.x && newHead.y === food.y) {
+		food = randomPosition()
+	} else {
+		snake.pop();
+	}
 	snake.unshift(newHead);
-	snake.pop();
 }
 
 function draw() {
-	ctx.clearRect(0, 0, canvasWidth, canvasHeight); // Очищаем канвас
+	ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 	//отрисовка сетки
 	function drawGrid() {
 		ctx.strokeStyle = "grey";
@@ -72,24 +78,38 @@ function draw() {
 	ctx.shadowBlur = 4;
 	//отрисовка змеи
 	for (let i = 0; i < snake.length; i++) {
-		ctx.fillStyle = "rgba(168, 168, 18, 0.5)"
+		ctx.fillStyle = (i === 0) ? "rgba(168, 168, 18, 0.5)" : "rgba(168, 100, 18, 0.5)";
 		ctx.fillRect(snake[i].x, snake[i].y, cageSize, cageSize)
-		ctx.strokeStyle = "black"
-		ctx.lineWidth = 2
-		ctx.strokeRect(
-			snake[i].x + ctx.lineWidth / 2,
-			snake[i].y + ctx.lineWidth / 2,
-			cageSize - ctx.lineWidth,
-			cageSize - ctx.lineWidth
-		)
+		ctx.strokeStyle = "black";
+		ctx.lineWidth = 2;
+		if (i === 0) {
+			ctx.strokeRect(
+				snake[i].x + ctx.lineWidth / 2,
+				snake[i].y + ctx.lineWidth / 2,
+				cageSize - ctx.lineWidth,
+				cageSize - ctx.lineWidth
+			)
+		}
 	}
+
+	//отрисовка еды
+	const centerX = food.x + cageSize / 2;
+	const centerY = food.y + cageSize / 2;
+	const radius = cageSize / 3;
+
+	ctx.beginPath(); 
+	ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
+	ctx.fillStyle = "red"; 
+	ctx.fill();  
+	ctx.strokeStyle = "black";
+	ctx.lineWidth = 2;     
+	ctx.stroke();         
 }
 
 function gameLoop() {
-	moveSnake(); // Перемещаем змею
-	draw(); // Отрисовываем игровую область
+	moveSnake();
+	draw(); 
 }
 
-// Запуск игрового цикла каждые 100 мс
-setInterval(gameLoop, 500);
+setInterval(gameLoop, 250);
 
