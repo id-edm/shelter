4060924fc5b4d42fab9539c4f7fc5ca8368394d3
Derevjannykh Ivan <ivandubochit@gmail.com>
2024-10-03 12:01:44 +0400

    feat: add snake movement & control buttons

--- random-game/main.js
+++ random-game/main.js
@@ -10,12 +10,49 @@ const position = {
 	y: Math.floor(Math.random() * (canvasHeight / cageSize)) * cageSize,
 }
 
-let snake = [{ x: position.x, y: position.y }]
+let snake = [{ x: position.x, y: position.y }];
+let directionSnake = {x: 0, y: 0 };
+
+function changeDirection(event) {
+	const key = event.key;
+	console.log(event)
+
+	switch (key) {
+		case "ArrowUp":
+			directionSnake = { x: 0, y: -cageSize };
+			break;
+		case "ArrowDown":
+			directionSnake = { x: 0, y: cageSize };
+			break;
+		case "ArrowLeft":
+			directionSnake = { x: -cageSize, y: 0 };
+			break;
+		case "ArrowRight":
+			directionSnake = { x: cageSize, y: 0 };
+			break;
+	}
+}
+
+document.addEventListener('keydown', changeDirection);
+
+function moveSnake() {
+	// получаем новую позицию головы змеи
+	const newHead = {
+			x: snake[0].x + directionSnake.x,
+			y: snake[0].y + directionSnake.y,
+	};
+
+	snake.unshift(newHead);
+	snake.pop();
+}
 
 function draw() {
+	ctx.clearRect(0, 0, canvasWidth, canvasHeight); // Очищаем канвас
+	//отрисовка сетки
 	function drawGrid() {
-		ctx.strokeStyle = "grey"
-		ctx.lineWidth = 2
+		ctx.strokeStyle = "grey";
+		ctx.lineWidth = 2;
+		ctx.shadowBlur = 0;
 		ctx.beginPath()
 		for (let i = 0; i <= canvasWidth; i += cageSize) {
 			ctx.moveTo(i, 0)
@@ -30,8 +67,10 @@ function draw() {
 
 	drawGrid()
 
-	ctx.shadowColor = "rgba(0,0,0,0.5)"
-	ctx.shadowBlur = 4
+  //тень головы змеи
+	ctx.shadowColor = "rgba(0,0,0,0.5)";
+	ctx.shadowBlur = 4;
+	//отрисовка змеи
 	for (let i = 0; i < snake.length; i++) {
 		ctx.fillStyle = "rgba(168, 168, 18, 0.5)"
 		ctx.fillRect(snake[i].x, snake[i].y, cageSize, cageSize)
@@ -46,4 +85,11 @@ function draw() {
 	}
 }
 
-draw()
+function gameLoop() {
+	moveSnake(); // Перемещаем змею
+	draw(); // Отрисовываем игровую область
+}
+
+// Запуск игрового цикла каждые 100 мс
+setInterval(gameLoop, 500);
+
